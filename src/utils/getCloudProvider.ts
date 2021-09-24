c6f00add2483fe37eafe80973110f51562489a5b
BangNguyen <nguyenbahaibang@gmail.com>
2021-09-24 13:43:28 +0300

    Updates

--- src/utils/getCloudProvider.ts
+++
@@ -1,8 +0,0 @@
-import { cloudProvider } from '../dictionary';
-
-function getCloudProvider(name: string): string {
-  const shortName = name.split('-')[0];
-  return cloudProvider[shortName];
-}
-
-export default getCloudProvider;
--- src/utils/getKeyByValue.ts
+++
@@ -1,3 +0,0 @@
-export default function getKeyByValue(object: Record<string, string>, value: string): string {
-  return Object.keys(object).find((key) => object[key] === value) as string;
-}
